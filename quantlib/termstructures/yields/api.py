e0c9be7745de1b3bea19739175b0daca46e2c271
Guillaume Horel <guillaume.horel@gmail.com>
2021-11-05 14:49:15 -0400

    add OvernightIndexFutureRateHelper

--- quantlib/termstructures/yields/api.py
+++ quantlib/termstructures/yields/api.py
@@ -7,6 +7,7 @@ from .piecewise_zerospreaded_termstructure import PiecewiseZeroSpreadedTermStruc
 from .rate_helpers import (
     RateHelper, DepositRateHelper, FraRateHelper,
     FuturesRateHelper, SwapRateHelper)
+from .overnightindexfutureratehelper import OvernightIndexFutureRateHelper, SofrFutureRateHelper
 from .discount_curve import InterpolatedDiscountCurve, DiscountCurve
 from ..yield_term_structure import YieldTermStructure
 from .zero_curve import ZeroCurve
