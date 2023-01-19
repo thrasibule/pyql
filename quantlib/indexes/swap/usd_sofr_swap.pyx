7513e45bcca293e8d4a096fcce6c8d54630e3930
Guillaume Horel <guillaume.horel@gmail.com>
2023-01-19 11:07:02 -0500

    add UsdSofrSwapIceFix

---
+++ quantlib/indexes/swap/usd_sofr_swap.pyx
@@ -0,0 +1,14 @@
+from cython.operator cimport dereference as deref
+from quantlib.time.date cimport Period
+from quantlib.termstructures.yield_term_structure cimport HandleYieldTermStructure
+from quantlib.indexes.swap_index cimport OvernightIndexedSwapIndex
+
+cimport quantlib.indexes.swap._usd_sofr_swap as _uss
+cimport quantlib._index as _in
+
+cdef class UsdSofrSwapIceFix(OvernightIndexedSwapIndex):
+    def __init__(self, Period tenor, HandleYieldTermStructure yts=HandleYieldTermStructure()):
+        self._thisptr.reset(
+            new _uss.UsdSofrSwapIceFix(deref(tenor._thisptr),
+                                       yts.handle)
+        )
