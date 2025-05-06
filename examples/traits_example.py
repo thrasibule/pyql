3192191da593ab1a0bc119c9bf1c2d6ec803933a
Guillaume Horel <guillaume.horel@gmail.com>
2025-05-06 15:41:22 -0400

    fix example

--- examples/traits_example.py
+++ examples/traits_example.py
@@ -4,17 +4,17 @@ import datetime
 from traits.api import HasTraits, Enum, Float, Date, Property, Range
 from traitsui.api import View, Item, HGroup, EnumEditor
 
-from quantlib.instruments.option import Put, Call, EuropeanExercise
+from quantlib.instruments.option import OptionType
+from quantlib.instruments.exercise import EuropeanExercise
 from quantlib.instruments.payoffs import PlainVanillaPayoff
 from quantlib.instruments.option import VanillaOption
-from quantlib.pricingengines.vanilla import AnalyticEuropeanEngine
+from quantlib.pricingengines.api import AnalyticEuropeanEngine
 from quantlib.processes.black_scholes_process import BlackScholesMertonProcess
 from quantlib.quotes import SimpleQuote
 from quantlib.settings import Settings
 from quantlib.time.api import TARGET, Actual365Fixed, today, Date as QlDate
-from quantlib.termstructures.yields.api import FlatForward
-from quantlib.termstructures.volatility.equityfx.black_vol_term_structure \
-    import BlackConstantVol
+from quantlib.termstructures.yields.api import FlatForward, HandleYieldTermStructure
+from quantlib.termstructures.volatility.api import BlackConstantVol
 
 
 settings = Settings.instance()
@@ -31,7 +31,7 @@ settings.evaluation_date = todays_date
 class OptionValuation(HasTraits):
 
     # options parameters
-    option_type = Enum(Put, Call)
+    option_type = Enum(OptionType.Put, OptionType.Call)
     underlying = Float(36)
     strike = Float(40)
     dividend_yield = Range(0.0, 0.5)
@@ -49,12 +49,6 @@ class OptionValuation(HasTraits):
 
     ### Traits view   ##########################################################
 
-    traits_view = View(
-        Item('option_type', editor=EnumEditor(values={Put:'Put', Call:'Call'})),
-        'underlying', 'strike', 'dividend_yield', 'risk_free_rate',
-        'volatility', 'maturity',
-        HGroup( Item('option_npv', label='Option value'))
-    )
 
     ### Private protocol   #####################################################
 
@@ -72,16 +66,20 @@ class OptionValuation(HasTraits):
         underlyingH = SimpleQuote(self.underlying)
 
         # bootstrap the yield/dividend/vol curves
-        flat_term_structure = FlatForward(
-            reference_date = settlement_date,
-            forward = self.risk_free_rate,
-            daycounter = self.daycounter
+        flat_term_structure = HandleYieldTermStructure(
+            FlatForward(
+                reference_date = settlement_date,
+                forward = self.risk_free_rate,
+                daycounter = self.daycounter
+            )
         )
 
-        flat_dividend_ts = FlatForward(
-            reference_date = settlement_date,
-            forward = self.dividend_yield,
-            daycounter = self.daycounter
+        flat_dividend_ts = HandleYieldTermStructure(
+            FlatForward(
+                reference_date = settlement_date,
+                forward = self.dividend_yield,
+                daycounter = self.daycounter
+            )
         )
 
         flat_vol_ts = BlackConstantVol(
@@ -104,10 +102,17 @@ class OptionValuation(HasTraits):
 
         return european_option.net_present_value
 
+traits_view = View(
+    Item('option_type', editor=EnumEditor(values={OptionType.Put:'Put', OptionType.Call:'Call'})),
+    'underlying', 'strike', 'dividend_yield', 'risk_free_rate',
+    'volatility', 'maturity',
+    HGroup( Item('option_npv', label='Option value'))
+)
+
 if __name__ == '__main__':
 
     model = OptionValuation()
-    model.configure_traits()
+    model.configure_traits(view=traits_view)
 
 
 ### EOF #######################################################################
