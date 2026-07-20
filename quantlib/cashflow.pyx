9d2028fe00cd9c3551832af5b6a490460966c216
Guillaume Horel <guillaume.horel@gmail.com>
2026-07-20 10:46:25 -0400

    docs: improve docstrings for cashflow module
    Extract documentation from QuantLib C++ cashflow.hpp header:
    - Expand CashFlow class docstring as abstract base for all cash flows
    - Document date, amount properties with notes from C++ docs
    - Document has_occured with its relationship to event handling
    - Add SimpleCashFlow class docstring
    - Add Leg class docstring describing it as a sequence of cash flows
    - Document items() and __len__() methods

--- quantlib/cashflow.pyx
+++ quantlib/cashflow.pyx
@@ -20,21 +20,59 @@ from quantlib.ext cimport shared_ptr, optional
 import_datetime()
 
 cdef class CashFlow:
-    """Abstract Base Class.
+    """Base class for cash flows.
 
-    Use SimpleCashFlow instead
+    This class is purely abstract and acts as a base class for the
+    actual cash flow implementations.
 
+    .. note::
+        Use :class:`SimpleCashFlow` for a concrete implementation.
     """
 
     @property
     def date(self):
+        """Returns the date that the cash flow occurs.
+
+        This is inherited from the Event class.
+
+        Returns
+        -------
+        :class:`~quantlib.time.date.Date`
+        """
         return date_from_qldate(self._thisptr.get().date())
 
     @property
     def amount(self):
+        """Returns the amount of the cash flow.
+
+        .. note::
+            The amount is not discounted, i.e., it is the actual
+            amount paid at the cash flow date.
+
+        Returns
+        -------
+        float
+        """
         return self._thisptr.get().amount()
 
     def has_occured(self, Date ref_date, include_ref_date=None):
+        """Returns ``True`` if the cash flow has already occurred.
+
+        Overloads ``Event.hasOccurred`` in order to take
+        ``Settings.includeTodaysCashflows`` into account.
+
+        Parameters
+        ----------
+        ref_date : :class:`~quantlib.time.date.Date`
+            The reference date.
+        include_ref_date : bool, optional
+            Whether to include the reference date in the comparison.
+            If ``None``, uses the global settings default.
+
+        Returns
+        -------
+        bool
+        """
         cdef _cf.CashFlow* cf = self._thisptr.get()
         cdef optional[bool] c_include_ref_date
         if include_ref_date is not None:
@@ -43,6 +81,17 @@ cdef class CashFlow:
             return cf.hasOccurred(ref_date._thisptr, c_include_ref_date)
 
 cdef class SimpleCashFlow(CashFlow):
+    """Concrete cash flow implementation.
+
+    A simple cash flow that pays a fixed amount at a given date.
+
+    Parameters
+    ----------
+    amount : float
+        The amount of the cash flow.
+    cfdate : :class:`~quantlib.time.date.Date`
+        The date of the cash flow.
+    """
 
     def __init__(self, Real amount, Date cfdate):
         self._thisptr = shared_ptr[_cf.CashFlow](
@@ -67,19 +116,41 @@ cdef list leg_items(const _cf.Leg& leg):
     return itemlist
 
 cdef class Leg:
+    """Sequence of cash flows.
+
+    A Leg is a vector of cash flows that can be iterated over.
+    It represents the payments leg of a financial instrument
+    (e.g., the fixed or floating leg of a swap).
+
+    Parameters
+    ----------
+    cashflows : list of :class:`CashFlow`
+        A list of cash flow objects.
+    """
 
     def __init__(self, cashflows: list[CashFlow]):
-        '''Takes as input a list of (amount, QL Date) tuples. '''
         cdef CashFlow cf
         for cf in cashflows:
             self._thisptr.push_back(cf._thisptr)
 
     def items(self):
-        '''Return Leg as (amount, date) list. '''
+        """Return the leg as a list of ``(amount, date)`` tuples.
+
+        Returns
+        -------
+        list of tuple
+            Each tuple contains ``(amount, pydate)``.
+        """
 
         return leg_items(self._thisptr)
 
     def __len__(self):
+        """Returns the number of cash flows in the leg.
+
+        Returns
+        -------
+        int
+        """
         cdef int size = self._thisptr.size()
         return size
 
