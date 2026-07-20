dd0e4d19866873a956b67aafd61a03c9012cce05
Guillaume Horel <guillaume.horel@gmail.com>
2026-07-20 10:47:09 -0400

    docs: improve docstrings for option and payoffs modules
    Extract documentation from QuantLib C++ option.hpp and payoffs.hpp:
    - Expand Option class docstring with definition of call/put options
    - Improve exercise/payoff property docstrings with Returns sections
    - Add Payoff abstract base class docstring and __call__ documentation
    - Add StrikedTypePayoff class docstring
    - Expand PlainVanillaPayoff docstring with mathematical formulas
    - Improve option_type and strike property docstrings

--- quantlib/option.pyx
+++ quantlib/option.pyx
@@ -4,7 +4,23 @@ from .exercise cimport Exercise
 from .payoffs cimport Payoff
 
 cdef class Option(Instrument):
-    """base option class"""
+    """Base option class.
+
+    An option is a financial derivative that gives the holder the right,
+    but not the obligation, to buy (Call) or sell (Put) an underlying
+    asset at a specified strike price on or before a specified date.
+
+    This is an abstract base class. Concrete implementations include
+    :class:`~quantlib.instruments.vanillaoption.VanillaOption`.
+
+    Attributes
+    ----------
+    Put : OptionType
+        Put option type.
+    Call : OptionType
+        Call option type.
+    """
+
     def __init__(self):
         raise NotImplementedError(
             'Cannot implement this abstract class. Use child like the '
@@ -21,14 +37,24 @@ cdef class Option(Instrument):
 
     @property
     def exercise(self):
-        """:class:`~quantlib.exercise.Exercise`"""
+        """Returns the exercise associated with the option.
+
+        Returns
+        -------
+        :class:`~quantlib.exercise.Exercise`
+        """
         cdef Exercise ex = Exercise.__new__(Exercise)
         ex._thisptr = (<_option.Option*>self._thisptr.get()).exercise()
         return ex
 
     @property
     def payoff(self):
-        """:class:`~quantlib.payoffs.Payoff`"""
+        """Returns the payoff associated with the option.
+
+        Returns
+        -------
+        :class:`~quantlib.payoffs.Payoff`
+        """
         cdef Payoff po = Payoff.__new__(Payoff)
         po._thisptr = (<_option.Option*>self._thisptr.get()).payoff()
         return po
--- quantlib/payoffs.pyx
+++ quantlib/payoffs.pyx
@@ -8,7 +8,11 @@ cdef inline _payoffs.PlainVanillaPayoff* _get_payoff(PlainVanillaPayoff payoff):
     return <_payoffs.PlainVanillaPayoff*> payoff._thisptr.get()
 
 cdef class Payoff:
+    """Abstract base class for option payoffs.
 
+    This class defines the interface for payoff functions. A payoff
+    can be called as a function of the underlying price at exercise.
+    """
 
     def __str__(self):
         if self._thisptr:
@@ -17,23 +21,46 @@ cdef class Payoff:
             raise ValueError("empty payoff")
 
     def __call__(self, Real price):
+        """Returns the payoff value for a given underlying price.
+
+        Parameters
+        ----------
+        price : float
+            The price of the underlying asset at exercise.
+
+        Returns
+        -------
+        float
+            The payoff value.
+        """
         return deref(self._thisptr)(price)
 
 
 cdef class StrikedTypePayoff(Payoff):
+    """Intermediate class for put/call payoffs.
+
+    Adds the concept of an option type (Call or Put) to the base
+    Payoff class.
+    """
     pass
 
 
 cdef class PlainVanillaPayoff(StrikedTypePayoff):
-    """ Plain vanilla payoff.
+    """Plain vanilla payoff.
+
+    The payoff for a plain vanilla option is:
+
+    - Call: :math:`\max(S - K, 0)`
+    - Put: :math:`\max(K - S, 0)`
+
+    where :math:`S` is the underlying price and :math:`K` is the strike.
 
     Parameters
     ----------
     option_type : :class:`~quantlib.option.OptionType`
-        The type of option, can be either `Call` or `Put`
-    strike : double
-        The strike value
-
+        The type of option, can be either ``Call`` or ``Put``.
+    strike : float
+        The strike value.
     """
 
     def __init__(self, OptionType option_type, double strike):
@@ -46,9 +73,20 @@ cdef class PlainVanillaPayoff(StrikedTypePayoff):
 
     @property
     def option_type(self):
-        """:class:`~quantlib.option.OptionType`"""
+        """Returns the option type (Call or Put).
+
+        Returns
+        -------
+        :class:`~quantlib.option.OptionType`
+        """
         return _get_payoff(self).optionType()
 
     property strike:
+        """Returns the strike value.
+
+        Returns
+        -------
+        float
+        """
         def __get__(self):
             return _get_payoff(self).strike()
